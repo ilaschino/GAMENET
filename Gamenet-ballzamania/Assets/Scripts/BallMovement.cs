92ad769e2595dbb6ad617c933ce8f5ce403aac2a
VIRGINITY-PROTE\Rowel <capulong.rowel@gmail.com>
2019-03-10 16:18:52 +0800

    Ball physics

---
+++ Gamenet-ballzamania/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour {
+
+    [SerializeField] private float MinSpeed = 2;
+    [SerializeField] private float ImpactForce = 10f;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.right * ImpactForce, ForceMode.Impulse);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 speed = rb.velocity;
+        Debug.Log(speed);
+
+        if (MinSpeed > speed.magnitude)
+        {
+            speed = rb.velocity.normalized * MinSpeed;
+            speed.y = rb.velocity.y;
+            rb.velocity = speed;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            Vector3 dir = collision.contacts[0].point - transform.position;
+            dir = -dir.normalized;
+
+            rb.AddForce(dir * ImpactForce, ForceMode.Impulse);
+        }
+    }
+}
---
+++ Gamenet-ballzamania/Assets/Scripts/SpawnerTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SpawnerTest : NetworkBehaviour {
+
+    [SerializeField] private GameObject ballPrefab;
+
+    private void Start()
+    {
+        if (isServer)
+            StartCoroutine(CmdSpawn());
+    }
+
+    private void Update()
+    {
+        print(isServer);
+    }
+
+    // Update is called once per frame
+    IEnumerator CmdSpawn()
+    {
+        GameObject ball = Instantiate(ballPrefab);
+        NetworkServer.Spawn(ball);
+
+        yield return new WaitForSeconds(3f);
+
+        StartCoroutine(CmdSpawn());
+    }
+}
