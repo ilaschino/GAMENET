b4c48f8ee9edb01c580592df4ce8e163632670c4
DESKTOP-4I2FNBM\OSAMU <osamu.umeda@benilde.edu.ph>
2019-04-16 17:46:50 +0800

    GAMENET

--- Gamenet-ballzamania/Assets/Scripts/PlayerScore.cs
+++ Gamenet-ballzamania/Assets/Scripts/PlayerScore.cs
@@ -2,19 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class PlayerScore : NetworkBehaviour {
 
+
     [SerializeField] private const int maxHealth = 5;
     [SyncVar]private int health = maxHealth;
+     
 
     public void OnChangehealth(int healthDeduction)
     {
         health -= healthDeduction;
         health = Mathf.Clamp(health, 0, maxHealth);
 
+        //if (health == 0 && isLocalPlayer)
+        //    StartCoroutine(DisplayLose());
+
         if (health == 0)
             StartCoroutine(DisplayLose());
+
+
+    }
+
+    private void Update()
+    {
+        
     }
 
     IEnumerator DisplayLose()
@@ -26,5 +39,9 @@ public class PlayerScore : NetworkBehaviour {
 
         Time.timeScale = 1;
         NetworkManager.singleton.StopClient();
+
+        Application.Quit();
+
+        
     }
 }
